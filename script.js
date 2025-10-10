f0996f443cfd050d821beca2d5731b378038fead
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-10-10 18:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor (species ){
+		this.species = species ;
+	}
 
-class Dog extends Animal {}
+	makeSound() {
+		console.log(`${species} make a sound`)
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
+
+class Cat extends Animal {
+	pour(){
+		console.log("pourr")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
