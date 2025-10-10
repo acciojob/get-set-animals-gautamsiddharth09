e2eed9eb6236b1ed0f1e3366c56c5556fb01e038
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-10-10 18:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
-//complete this code
 class Animal {
-	constructor (species ){
-		this._species = species ;
-	}
+  constructor(species) {
+    this._species = species; // use _species to avoid getter conflict
+  }
 
-	get species () {
-		return this._species;
-	}
+  get species() {
+    return this._species;
+  }
 
-	makeSound() {
-		console.log(`The ${this._species} make a sound`)
-	}
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
 class Dog extends Animal {
-	constructor(species){
-		super(species)
-	}
+  constructor(species) {
+    super(species);
+  }
 
-	bark(){
-		console.log("woof")
-	}
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	constructor(species){
-		super(species)
-	}
-	pourr(){
-		console.log("pourr")
-	}
+  constructor(species) {
+    super(species);
+  }
+
+  purr() { // fixed method name
+    console.log("purr");
+  }
 }
 
+// Example usage
 const myCat = new Cat("Siamese");
-myCat.makeSound();
+myCat.makeSound(); // The Siamese makes a sound
+myCat.purr();      // purr
 
-// Do not change the code below this line
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // The Golden Retriever makes a sound
+myDog.bark();      // woof
+
+// Do not change the code below
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
